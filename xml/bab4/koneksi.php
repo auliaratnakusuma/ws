241a1598968d0046ce7dc2c366cbccdcbe15f7c7
auliaratna <aulia.ratna.k@mail.ugm.ac.id>
2014-10-01 08:40:59 +0700

    add koneksi.php

--- xml/bab4/koneksi.php
+++ xml/bab4/koneksi.php
@@ -2,5 +2,5 @@
 //koneksi database
 $link = mysql_connect('localhost','root', '')
 or die('Could not connect: '. mysq_error());
-mysql_select_db('data_mahasiswa')or die('Could not selet database');
+mysql_select_db('mahasiswa')or die('Could not selet database');
 ?>
