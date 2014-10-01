bb0dad9902fe607bf43d276f3aa0761b16484b2f
auliaratna <aulia.ratna.k@mail.ugm.ac.id>
2014-10-01 08:40:00 +0700

    add mhs.php

--- xml/bab4/mhs.php
+++ xml/bab4/mhs.php
@@ -2,43 +2,53 @@
 //header untuk format xml, jika dihilangkan maka akan berbentuk data string
 header('Content-Type: text/xml; charset=ISO-8859-1');
 include "koneksi.php";
+$path_params = array();
+$self = $_SERVER['PHP_SELF'];
+$extension = substr($self, strlen($self)-3);
+$path = ($extension=='php') ? NULL : $_SERVER['PATH_INFO'];
 
 //check for the part element
-$path = $_SERVER[PATH_INFO];
+//$path = $_SERVER[PATH_INFO];
 if ($path != null){
-	$path_params = spliti ("/", $path);
+	$path_params = explode ("/", $path); // explode digunakan untuk parsing ngambil data yng punya nim 123 http://localhost/ws.git/xml/bab4/mhs.php/123 
 	}
 	
 //metode request untuk GET
  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	if ($path_params[1] !=null){
+	if (isset($path_params[1]) && $path_params[1] != NULL ){
 		$query = "select
 			nim,
 			nama,
 			alamat,
-			prodi,
+			prodi
 			from mahasiswa where nim = $path_params[1]";
 		} else{
 			$query = "select
 			nim,
 			nama,
 			alamat,
-			prodi,
+			prodi
 			from mahasiswa";
 			
 		}
 		$result = mysql_query($query) or die ('Query failed:'. mysql_error());
 		
 		echo "<data>";
-		while ($line = mysql_fetch_arra($result, MYSQL_ASSOC)) {
+		while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 			echo "<mahasiswa>";
-			foreach ($line as $key => $col_value) (
+			foreach ($line as $key => $col_value) {
+		echo "<$key>$col_value</$key>";
+		}
+		echo "</mahasiswa>";
+		}
 		echo "</data>";
 		
 		mysql_free_result($result);
 	}
 	//metode request untuk delete
-	}
+	
+	
 	mysql_close($link);
+	
 ?>
 			
